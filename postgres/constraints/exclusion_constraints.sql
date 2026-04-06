c1adcb854559293cbcbfa518a16f0f7a63b0d813
Muhammad Awab <awabhero34@gmail.com>
2026-04-06 19:33:19 +0500

    Add exclusion constraints for overlapping appointments

--- postgres/constraints/exclusion_constraints.sql
+++ postgres/constraints/exclusion_constraints.sql
@@ -8,4 +8,20 @@ EXCLUDE USING gist (
     timerange(shift_start, shift_end) WITH &&
 );
 
-  
+ALTER TABLE appointments 
+ADD CONSTRAINT no_overlapping_appointments_doctors
+EXCLUDE USING gist
+(
+    doctor_id WITH =,
+    scheduled_at =,
+    tstzrange(scheduled_at, scheduled_at + (duration_hours * 'Interval 1 hour'))
+);
+
+ALTER TABLE appointments
+ADD CONSTRAINT no_overlapping_appointments_patients
+EXCLUDE USING gist
+(
+    patient_id WITH =,
+    scheduled_at =,
+    tstzrange(scheduled_at, scheduled_at + (duration_hours * 'Interval 1 hour'))
+);
