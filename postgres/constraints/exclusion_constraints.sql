aa6fad8475fc93a5238d96a0a0268bb60ee1737a
Muhammad Awab <awabhero34@gmail.com>
2026-04-06 19:28:58 +0500

    Update exclusion_constraints.sql

--- postgres/constraints/exclusion_constraints.sql
+++ postgres/constraints/exclusion_constraints.sql
@@ -1,1 +1,11 @@
+CREATE EXTENSION IF NOT EXISTS btree_gist;
 
+ALTER TABLE doctor_schedule
+ADD CONSTRAINT no_overlapping_shifts 
+EXCLUDE USING gist (
+    doctor_id WITH =, 
+    day WITH =, 
+    timerange(shift_start, shift_end) WITH &&
+);
+
+  
