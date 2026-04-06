7666bbaa38f830706c634c209ebde5b27378857d
Muhammad Awab <awabhero34@gmail.com>
2026-04-06 19:21:28 +0500

    Remove unique indexes from patients and nurses tables
    Removed several unique indexes related to patients, nurses, and appointments.

--- postgres/root/indexes.sql
+++ postgres/root/indexes.sql
@@ -1,20 +1,3 @@
-CREATE UNIQUE INDEX unique_patient_active_ward 
-ON patients_in_wards (patient_id) 
-WHERE discharged_at IS NULL;    
-
-CREATE UNIQUE INDEX unique_nurse_active_ward_per_ward
-ON nurses_in_wards (nurse_id, ward_name) 
-WHERE discharged_at IS NULL;    
-
-CREATE UNIQUE INDEX patient_doctor_unique_appointment
-ON appointments (doctor_id, patient_id)
-WHERE "status" = 'scheduled';
-
-CREATE UNIQUE INDEX ON nurses_salary(nurse_id, "month", "year");
-
-CREATE UNIQUE INDEX ON doctor_schedule (doctor_id,"day");
-
-
 CREATE INDEX i0 
 ON profiles(id) INCLUDE (first_name, last_name);
 
